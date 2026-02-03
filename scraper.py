7a48f0cc9ba36a37214e86fa8cbeffaa9c714bb4
1anArr3d <ianarredondo2004@gmail.com>
2026-02-03 02:09:02 -0600

    Add SQLite database and refactor scraper to store results

--- scraper.py
+++ scraper.py
@@ -1,14 +1,16 @@
 from playwright.sync_api import sync_playwright
 import time
+import json
 
 base_url = "https://app.marketplace.autura.com"
 start_time = time.perf_counter()
 
 def scrape_data(auctionid, city):
+    data = []
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
+        
         page = browser.new_page()
-
         block_types = {"image", "media", "font",}
         page.route("**/*", lambda r: r.abort() if r.request.resource_type in block_types else r.continue_())
 
@@ -28,18 +30,25 @@ def scrape_data(auctionid, city):
             table_block = vehicle_page.wait_for_selector("div.ant-table-content")
             rows = table_block.query_selector_all("tr")
 
+            vehicle_data = {}
             for row in rows:
                 cells = row.query_selector_all("td")
                 name = cells[0].inner_text().strip()
                 value = cells[1].inner_text().strip()
-                print(f"{name}: {value}")
+                vehicle_data[name] = value
+            
+            data.append(vehicle_data)
 
         # Close pages and browser after loop
         vehicle_page.close()
         page.close()
         browser.close()
+    
+    return data
 
-scrape_data(108309, "SA-TX")
+vehicles = scrape_data(108767, "SA-TX")
+vehicles_json = json.dumps(vehicles, indent=2)
+print(vehicles_json)
 
 end_time = time.perf_counter()
 print(f"Runtime: {end_time - start_time:.3f} seconds")
